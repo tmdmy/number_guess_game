02d00b69e2b63b02191398a79350cc15c9f30314
tmdmy <tmdmymy@gmail.com>
2025-08-09 15:03:51 +0900

    ヒント機能を追加

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ while guess != number:
     guess = int(input("1〜100の数字を当ててください: "))
     if guess < number:
         print("もっと大きいです")
+        if number - guess < 10:
+            print("かなり近い！")
     elif guess > number:
         print("もっと小さいです")
+        if guess - number < 10:
+            print("かなり近い！")
     else:
         print("正解！")
+
